9b50c7492943c0b7d294ffe57b3500d102c9c882
Diego <94410736+DiegoEzequiel-02@users.noreply.github.com>
2025-06-21 16:38:39 -0300

    Update Punto 5

--- EBgdtlpe.pl
+++ EBgdtlpe.pl
@@ -1,36 +1,47 @@
-hombre(damian, alto, castaño, adulto).
-hombre(raul, medio, rubio, joven).
-hombre(juan, bajo, negro, adulto).
+%Donde N es el nombre de un hombre o una mujer, 
+%A su altura (alta, media, baja), 
+%C el color de su cabello (rubio, castaño, pelirrojo, negro), 
+%E su edad (joven,adulta,vieja).
 
-mujer(natalia, alta, pelirrojo, joven).
-mujer(sofia, baja, castaño, adulta).
-mujer(marta, media, negro, adulta).
+hombre(juan,alta,rubio,joven).
+hombre(pedro,baja,pelirrojo,adulta).
+hombre(ramiro,media,castaño,vieja).
+mujer(paula,media,negro,adulta).
+mujer(sofia,alta,rubio,joven).
+mujer(silvia,baja,pelirrojo,vieja).
 
-gusta(damian, jazz, aventura, tenis).
-gusta(raul, pop, ciencia-ficcion, natacion).
-gusta(juan, clasica, policiaca, jogging).
+%Que indica que a la persona N le gusta el género de música M 
+%(clásica, pop, jazz), el género de literatura L 
+%(aventura, ciencia-ficción, policíaca), y 
+%practica el deporte S (tenis, natación, jogging).
 
-gusta(natalia, pop, aventura, tenis).
-gusta(sofia, clasica, ciencia-ficcion, natacion).
-gusta(marta, jazz, policiaca, jogging).
+gusta(pedro,jazz,policíaca,natación).
+gusta(silvia,jazz,policíaca,natación).
+gusta(sofia,pop,aventura,tenis).
+gusta(juan,clásica,policíaca,jogging).
 
-busca(damian, baja, castaño, joven).
-busca(raul, alta, pelirrojo, adulta).
-busca(juan, media, negro, joven).
+%Que expresa que la persona N busca una pareja de altura A, 
+%con cabello color C y edad E.
 
-busca(natalia, medio, rubio, adulto).
-busca(sofia, bajo, castaño, joven).
-busca(marta, alto, negro, adulto).
+busca(paula,alta,castaño,joven).
+busca(silvia,baja,pelirrojo,adulta).
+busca(ramiro,baja,pelirrojo,adulta).
+busca(juan,media,rubio,vieja).
+busca(pedro,baja,pelirrojo,vieja).
 
 %Se considera que dos personas x e y de sexos diferentes 
 %son adecuadas si x conviene a y e y conviene a x. 
-
 %Se dice que x conviene a y si x conviene físicamente a y 
-%(la altura, edad y color de cabello de y son los que busca x) 
-%y si, además, los gustos de x e y en música, literatura y deporte coinciden.
+%(la altura, edad y color de cabello de y son los que busca x) y si, 
+%además, los gustos de x e y en música, literatura y deporte coinciden.
 
-adecuadas(x,y):- 
-    busca(x,Altura, Cabello, Edad),
-    (hombre(y,Altura,Cabello,Edad);mujer(y,Altura,Cabello,Edad)),
-    x\= y.
-
+conviene(X,Y):-
+    hombre(X,AX,CX,EX),
+    mujer(Y,AY,CY,EY),
+    busca(X,AY,CY,EY),
+    busca(Y,AX,CX,EX),
+    gusta(X,MX,LX,SX),
+    gusta(Y,MY,LY,SY),
+    MX == MY,
+    LX == LY,
+	SY == SX.
