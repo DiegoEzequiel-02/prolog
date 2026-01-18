aea07e36e447d8a87424e47f05d41875b99f635b
Diego <94410736+DiegoEzequiel-02@users.noreply.github.com>
2026-01-18 13:00:52 -0300

    Punto 4 de listas

--- ygpunRlb.pl
+++ ygpunRlb.pl
@@ -7,3 +7,12 @@ largo([_ | L], N) :-
 
 solucion_miembro(X,[X|_]).
 solucion_miembro(X,[_|T]):-solucion_miembro(X,T).
+
+%Pudiendo utilizar predicados existentes, resolver
+%Para un listado de 0 a n hechos nota/1, con notas de 1 a 10, hallar el promedio de las mismas. promedio_nota/1
+obtener_cant([],0).
+obtener_cant([_|T],C):- obtener_cant(T,C1), C is C1+1.
+suma([],0).
+suma([H|T],R):- H >= 1, H =< 10, suma(T,C1), R is C1 + H.
+promedio_nota([]).
+promedio_nota([H|T]):- obtener_cant([H|T],C),H > 0,suma([H|T],S), P is S/C, write(P).
